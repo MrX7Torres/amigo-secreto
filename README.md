d2b1eb0bf6d4eb8eef90cf0ea7357278fe0822f5
Jesus Torres <jesusantoniotorres316@gmail.com>
2025-08-30 12:59:31 -0600

    Proyecto ONE, solo falta el README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # amigo-secreto
 Implementacion de juego del amigo secreto donde el objetivo es poder anadir nombres para al final se elija uno al azar.
+<h1 align ="center"> Juego de Amigo Secreto para el curso ONE </h1>
--- app.js
+++ app.js
@@ -2,18 +2,39 @@
 let amigos = []
 
 function limpiarCaja() {
-    document.querySelector('amigo').value = '';
+    document.getElementById('amigo').value = '';
 }
 
 function agregarAmigo() {
-    let nombrePorUsuario = getElementById('amigo').value;
+    let nombrePorUsuario = document.getElementById('amigo').value;
     if (nombrePorUsuario == ''){
         alert("Por favor, inserte un nombre.");
-        return agregarAmigo();
+    } else if (amigos.includes(nombrePorUsuario)){
+        alert("Este nombre ya existe en la lista, inserte otro nombre.");
     } else {
         amigos.push(nombrePorUsuario);
-        return nombrePorUsuario;
     }
     limpiarCaja();
+    mostrarAmigos();
+}
+
+function mostrarAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        return;
+    } else {
+        let amigoAleatorio = amigos[Math.floor(Math.random()*amigos.length)];
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = amigoAleatorio;
+    }
 }
 
